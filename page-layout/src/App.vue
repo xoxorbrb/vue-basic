b17d669264b7aad2e7c55604a5566458221e19f2
xoxorbrb <tg3782@naver.com>
2022-06-13 01:16:56 +0900

    Component 구성해보기

--- page-layout/src/App.vue
+++ page-layout/src/App.vue
@@ -1,13 +1,33 @@
 <template>
+<div>
+  <MyHeader/>
+  <div class = "wrap">
+    <MyMenu/>
+    <MyContent/>
+  </div>
+</div>
 </template>
 
 <script>
+import MyContent from '@/components/MyContent';
+import MyHeader from '@/components/MyHeader';
+import MyMenu from '@/components/MyMenu';
 
 export default {
-  name: 'App',
-  components: {}
-}
+  name: "app" , 
+  components: {
+    MyContent,
+    MyHeader,
+    MyMenu
+  }
+};
 </script>
 
-<style>
+<style scoped>
+h1 {
+  color: #03a9f4
+}
+.wrap {
+  display: flex;
+}
 </style>
---
+++ page-layout/src/components/MyHeader.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        Header Component
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style scoped>
+    div {
+        position: sticky;
+        height: 50px;
+        border-bottom: 1px solid #ebebeb;
+    }
+</style>
