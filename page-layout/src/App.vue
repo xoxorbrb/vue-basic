57f02849be3775d9ebd2a62495b57deab93fd89e
xoxorbrb <tg3782@naver.com>
2022-06-13 22:22:57 +0900

    State 와 Props

--- page-layout/src/App.vue
+++ page-layout/src/App.vue
@@ -6,19 +6,28 @@
     <MyContent/>
   </div>
 </div>
+<div>
+  <MyBox color = "blue"/>
+  <MyBox color = "purple"/>
+  <MyBox color = "green"/>
+  <MyBox color = "blue"/>
+  <MyBox color = "purple"/>
+  <MyBox/>
+</div>
 </template>
 
 <script>
 import MyContent from '@/components/MyContent';
 import MyHeader from '@/components/MyHeader';
 import MyMenu from '@/components/MyMenu';
-
+import MyBox from '@/components/MyBox';
 export default {
   name: "app" , 
   components: {
     MyContent,
     MyHeader,
-    MyMenu
+    MyMenu,
+    MyBox
   }
 };
 </script>
---
+++ page-layout/src/components/MyBox.vue
@@ -0,0 +1,30 @@
+<template>
+    <div :class="['box', color]" :style = "{width: width + 'px', height: height + 'px'}"></div>
+</template>
+<script>
+export default {
+    props: {
+        color: { type: String, default: ""}
+    },
+    data() {
+        return {
+            width: 40,
+            height: 80,
+        };
+    }
+};
+</script>
+<style scoped>
+    .box {
+        border: 1px solid #000;
+    }
+    .blue {
+        background: #0099;
+    }
+    .purple {
+        background: #8f46ff;
+    }
+    .green {
+        background: #00bcac;
+    }
+</style>
